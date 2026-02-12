92133910db13a112fb296b9b3ae623748c5d8af7
pedronascr <pedrohnrosa@gmail.com>
2026-02-12 16:07:08 +0000

    TESTE DE IMAGEM

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Eu amo gatos!</h1>
+    return <img src="https://tse2.mm.bing.net/th/id/OIP.dnVupN1QHLwm69ORI8mICgHaJ4?rs=1&pid=ImgDetMain&o=7&rm=3" alt="Descrição" />
 }
 
 export default Home;
